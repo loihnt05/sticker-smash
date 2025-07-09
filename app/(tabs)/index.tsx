f51eb2fbf129bcee49b6459a81bab24d4fab374a
loihnt05 <23520869@gm.uit.edu.vn>
2025-07-09 22:09:16 +0700

    Build a screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,18 @@
-import { Image } from 'expo-image';
-import { StyleSheet, View } from 'react-native';
+import Button from "@/app-example/components/Button";
+import ImageViewer from "@/app-example/components/ImageViewer";
+import { StyleSheet, View } from "react-native";
 
-
-const PlaceholderImage = require('@/assets/images/background-image.png');
-
+const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -18,15 +21,14 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
   },
   imageContainer: {
     flex: 1,
   },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
